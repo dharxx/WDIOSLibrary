cd827ec1acec63bee2a0a30074d3d42d3f4d9809
Dhanu Saksrisathaporn <DS@admins-imac.local>
2016-09-15 13:19:51 +0700

    update

--- Example/Pods/Target Support Files/JWT/JWT-umbrella.h
+++ Example/Pods/Target Support Files/JWT/JWT-umbrella.h
@@ -3,12 +3,11 @@
 #import "JWTAlgorithm.h"
 #import "JWTAlgorithmFactory.h"
 #import "JWTAlgorithmNone.h"
+#import "JWTAlgorithmDataHolder.h"
 #import "JWTAlgorithmHS256.h"
 #import "JWTAlgorithmHS384.h"
 #import "JWTAlgorithmHS512.h"
 #import "JWTAlgorithmHSBase.h"
-#import "JWTAlgorithmHSFamily.h"
-#import "JWTAlgorithmRS256.h"
 #import "JWTAlgorithmRSBase.h"
 #import "JWTRSAlgorithm.h"
 #import "JWTClaim.h"
@@ -16,6 +15,7 @@
 #import "JWTClaimsSetSerializer.h"
 #import "JWTClaimsSetVerifier.h"
 #import "JWT.h"
+#import "JWTDeprecations.h"
 
 FOUNDATION_EXPORT double JWTVersionNumber;
 FOUNDATION_EXPORT const unsigned char JWTVersionString[];
--- Example/Pods/Target Support Files/WDIOSLibrary/WDIOSLibrary-umbrella.h
+++ Example/Pods/Target Support Files/WDIOSLibrary/WDIOSLibrary-umbrella.h
@@ -1,9 +1,23 @@
-#import "Macro.h"
-#import "WDJWTString.h"
+#import <UIKit/UIKit.h>
 
-#import "WDRequestMaker.h"
-#import "WDIOSDataViewerHeader.h"
 #import "CategoryHeader.h"
+#import "Macro.h"
+#import "NSLocale+WDLocalized.h"
+#import "NSObject+MVCSupport.h"
+#import "NSString+WDLocalized.h"
+#import "UIFont+WDMoreFonts.h"
+#import "UIImage+StupidApplePhotoFix.h"
+#import "UITableView+MVCSupport.h"
+#import "UIView+MVCSupport.h"
+#import "UIViewController+MVCSupport.h"
+#import "WDIOSCollectionViewController.h"
+#import "WDIOSCollectionViewLayout.h"
+#import "WDIOSDataViewerHeader.h"
+#import "WDIOSTableViewController.h"
+#import "WDIOSTableWaitingView.h"
+#import "WDIOSWaitingView.h"
+#import "WDJWTString.h"
+#import "WDRequestMaker.h"
 
 FOUNDATION_EXPORT double WDIOSLibraryVersionNumber;
 FOUNDATION_EXPORT const unsigned char WDIOSLibraryVersionString[];
